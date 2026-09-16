3f7abd711400577abeceabed74e6a10afd52ad7d
Chopy5 <miguel.garcia0@rai.usc.es>
2026-09-16 10:02:21 +0200

    Initial commit

---
+++ src/main/java/gal/usc/etse/es/servicios/ProyectoServicioApplication.java
@@ -0,0 +1,13 @@
+package gal.usc.etse.es.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoServicioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gal/usc/etse/es/servicios/ProyectoServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package gal.usc.etse.es.servicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoServicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
